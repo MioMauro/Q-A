5303ab37ea6f6ccaa68d0b7c11088fbbc65c49ae
Mauro.V <101286831+MioMauro@users.noreply.github.com>
2022-09-17 07:41:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+Show/Hidden
+
+const questions = document.querySelectorAll(".question")
+
+questions.forEach((question) => {
+    const btn = question.querySelector(".question-btn")
+    btn.addEventListener("click", () => {
+        question.classList.toggle("show-text")
+    })
+})
+*/ 
+
+
+
+
+const questions = document.querySelectorAll(".question")
+
+questions.forEach((question) => {
+    const btn = question.querySelector(".question-btn")
+    btn.addEventListener("click", () => {
+        question.classList.toggle("show-text")
+    })
+})
